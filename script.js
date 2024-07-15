841c5907db7c491d40198ae9e9067ccc27f57328
IAmGit@06 <aakashkumarsumanaksgi7@gmail.com>
2024-07-15 22:22:58 +0530

    added locomotive js in order to smooth the scrolling

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ menuIcon.addEventListener("click", function(){
     menuIcon.classList.toggle('fa-x');
     navBar.classList.toggle('active');
 })
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
